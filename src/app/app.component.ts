18d193823cc3c64a19fe07bcac97d381c4bb52f6
Tiago <tiagocepeg@gmail.com>
2018-01-27 18:12:48 -0200

    Add data.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import {HttpClient} from '@angular/common/http';
+import * as data from './data.json';
 
 
 @Component({
@@ -18,6 +19,14 @@ export class AppComponent implements OnInit {
   }
   
   ngOnInit(): void {
+
+    const cidades = (<any>data).cidades;
+    const estados = (<any>data).estados;
+    const diversao = (<any>data).diversao;
+    console.log(cidades);
+    console.log(estados);
+    console.log(diversao);
+
     //this.http.get('https://api.hgbrasil.com/weather/?format=json&woeid=455903').subscribe(data => {
       this.http.get('api/weather/?format=json&woeid=455903').subscribe(data => {
       // Read the result field from the JSON response.
