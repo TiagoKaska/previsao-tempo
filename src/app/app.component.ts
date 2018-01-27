f736e93596fc374cf63cb2fa3c012b0f2ca2dce1
Tiago <tiagocepeg@gmail.com>
2018-01-27 17:37:48 -0200

    Iniciado App. Adicionado material.angular.io. Add configuração de proxy para api https://api.hgbrasil.com/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+//https://api.hgbrasil.com/weather/?format=json&woeid=455846
+export class AppComponent implements OnInit {
+  results: string[];
+
+  constructor(private http: HttpClient) {
+    
+  }
+  
+  ngOnInit(): void {
+    //this.http.get('https://api.hgbrasil.com/weather/?format=json&woeid=455903').subscribe(data => {
+      this.http.get('api/weather/?format=json&woeid=455903').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+      console.log(this.results)
+    });
+  }
+
+
+  cidadeFavorita = 'Blumenau';
+  checked = false;
+  indeterminate = false;
+  align = 'start';
+  disabled = false;
 }
